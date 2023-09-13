33162b77d7b70168e8e2acb5698c0f1218015c37
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-09-13 10:17:53 +0600

    v1.1.5

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
-a = [1, 7, 2]
+calories = {"day1": 420, "day2": 380, "day3": 390}
 
-myvar = pd.Series(a, index=["x", "y", "z"])
+myvar = pd.Series(calories)
 
 print(myvar)
 
-print(myvar["y"])
+print("------------------------------------------")
+
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+myvar = pd.DataFrame(data)
+
+print(myvar)
