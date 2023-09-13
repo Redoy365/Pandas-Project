b4dcfec357ee5aa51476bd213f7f71d9af526341
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-09-13 16:28:39 +0600

    v1.2.6

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('data.csv')
 
+
+print("----------------head()-------------------")
 print(df.head())
+print("---------------tail()---------------------")
+print(df.tail())
