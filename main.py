e2a66f312930a83b020404e13b260af79c2df797
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-09-13 10:08:34 +0600

    v1.1.4

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ import pandas as pd
 
 a = [1, 7, 2]
 
-myvar = pd.Series(a)
+myvar = pd.Series(a, index=["x", "y", "z"])
 
 print(myvar)
 
-# =========================
-
-b = [[1,3,5],[2,4,6]]
-
-bvar = pd.Series(b)
-
-print(bvar)
-print(bvar[0][1])
+print(myvar["y"])
