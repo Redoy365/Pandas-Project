ea62c064379dc8c260c486a79d1c9fa725f09383
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-04 11:14:44 +0600

    Key/Value Objects as Series

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-a = [1, 7, 2]
+calories = {"day1": 420, "day2": 380, "day3": 390}
 
-myvar = pd.Series(a, index = ["x", "y", "z"])
+myvar = pd.Series(calories)
 
 print(myvar)
